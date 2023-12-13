e6650acbf838fe5f85f6ff1f103247f896972995
Roshleen <roshleen.mand.5@gmail.com>
2023-12-13 15:29:25 +0530

    Added the User Controller

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,14 +1,14 @@
 const express = require("express");
+const { 
+    registerUser,
+    currentUser,
+    loginUser 
+} = require("../controllers/userController");
 const router = express.Router();
 
-router.post("/register", (req, res)=>{
-    res.json({message: "Register the user"});
-});
-router.post("/login", (req, res)=>{
-    res.json({message: "login user"});
-});
-router.get("/current", (req, res)=>{
-    res.json({message: "Current user information"});
-});
+router.post("/register", registerUser);
+
+router.post("/login", loginUser);
+router.get("/current", currentUser);
 
 module.exports = router;
