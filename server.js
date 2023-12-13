ce0b6a69cf67caf0cc9f92fa4ded57614efecc78
Roshleen <roshleen.mand.5@gmail.com>
2023-12-13 12:02:15 +0530

    CRUD: Getting Contact

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const port= process.env.PORT || 5000;
 
 app.use(express.json());
 app.use("/api/contacts", require("./routes/contactRoutes"));
-// app.use(errorHandler);
+app.use(errorHandler);
 
 // console.log("hi")
 // app.get('/hi', (req, res) => {
