b9ec973087f1d8859deaeebb60796281804440eb
Roshleen <roshleen.mand.5@gmail.com>
2023-12-18 11:14:04 +0530

    Validated the token on all the routes

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -7,7 +7,7 @@ const {
     updateContact, 
     deleteContact,
 } = require("../controllers/contactController");
-
+router.use(validateToken);
 router.route("/").get(getContacts).post(createContact);
 router.route("/:id").get(getContact).put(updateContact).delete(deleteContact);
 
