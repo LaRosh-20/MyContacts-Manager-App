909cd8d6490f297b3ae24f4cdbb4ac0e169f4ea7
Roshleen <roshleen.mand.5@gmail.com>
2023-12-18 11:17:05 +0530

    "Geting" the contacts for all the "Logged In" users

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -3,14 +3,14 @@ const Contact = require("../models/contactModel");
 const ContactDetail = require("../models/contactModel");
 //@desc  Get all contacts
 //@route Get /api/contacts
-//@access public
+//@access private
 const getContacts = asyncHandler(async (req, res) => {
-    const contacts = await Contact.find();
+    const contacts = await Contact.find({user_id: req.user.id});
     res.status(200).json(contacts);
 });
 //@desc  Create New contact
 //@route POST /api/contacts
-//@access public
+//@access private
 const createContact = asyncHandler(async (req, res) => {
     console.log("The request body is :", req.body);
     const { name, email, phone } = req.body;
@@ -27,7 +27,7 @@ const createContact = asyncHandler(async (req, res) => {
 });
 //@desc  Get contact
 //@route Get /api/contacts/:id
-//@access public
+//@access private
 const getContact = asyncHandler(async (req, res) => {
     const contact = await Contact.findById(req.params.id);
     if(!contact){
@@ -38,7 +38,7 @@ const getContact = asyncHandler(async (req, res) => {
 });
 //@desc  Update contact
 //@route PUT /api/contacts/:id
-//@access public
+//@access private
 const updateContact = asyncHandler(async (req, res) => {
     const contact = await Contact.findById(req.params.id);
     if(!contact){
@@ -54,7 +54,7 @@ const updateContact = asyncHandler(async (req, res) => {
 });
 //@desc  Delete contact
 //@route DELETE /api/contacts/:id
-//@access public
+//@access private
 const deleteContact = asyncHandler(async (req, res) => {
     const contact = await Contact.findById(req.params.id);
     if(!contact){
