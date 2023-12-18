cc7c06e98796eefc509e8ea2854a57e28829d61d
Roshleen <roshleen.mand.5@gmail.com>
2023-12-18 11:05:11 +0530

    Associated the contact with respective ID!!

--- models/contactModel.js
+++ models/contactModel.js
@@ -1,18 +1,24 @@
 const mongoose = require("mongoose");
-const contactSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: [true, "Please add the contact name"]
-    },
-    email: {
-        type: String,
-        required: [true, "Please add the email "]
-    },
-    phone: {
-        type: String,
-        required: [true, "Please add the contact phone number"]
-    }
-}, {
+const contactSchema = new mongoose.Schema(
+    {
+        user_id: {
+            type: mongoose.Schema.Types.ObjectId,
+            required: true,
+            ref: "User",
+        },
+        name: {
+            type: String,
+            required: [true, "Please add the contact name"]
+        },
+        email: {
+            type: String,
+            required: [true, "Please add the email "]
+        },
+        phone: {
+            type: String,
+            required: [true, "Please add the contact phone number"]
+        }
+    }, {
     timestamps: true,
 })
 
